0897e61c1dd56a4dbcc20b2db7ef281ac5f726f8
Joel <joel@hamsterpaj.net>
2009-04-08 22:29:47 +0200

    Made a lot of changes to open_source/index.php (in the text).
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- open_source/index.php
+++ open_source/index.php
@@ -1,41 +1,6 @@
 <?php
 	require('../include/core/common.php');
 	require(PATHS_LIBRARIES . 'open_source.lib.php');
-  /* OPEN_SOURCE */
-
-	$file_structure = array(
-		'open_source' => array(
-			'readfile.php',
-			'index.php',
-			'standard.php'
-		),
-		
-		'include' => array(
-			'libraries' => array(
-				'discussion_forum.lib.php',
-				'photos.lib.php',
-				'profile.lib.php'
-			),
-			
-			'ui-functions.php',
-			'shared-functions.php'
-		),
-		
-		'hamsterpaj' => array(
-			'nytt.php'
-		),
-		
-		'diskussionsforum' => array(
-			'index.php'
-		),
-		
-		'traffa' => array(
-			'profile.php',
-			'photos.php'
-		),
-		
-		'index.php'
-	);
 	
   $open_source_config['open_source_menu_path'] = 'start';
   $open_source_config['title'] = 'Open source-startsidan på Hamsterpaj.net';
@@ -57,33 +22,17 @@
   
   echo '<h2>Koda själv</h2>' . "\n";
   echo '<p>' . "\n";
-  echo '<a href="http://www.hamsterpaj.net/open_source/site_2008_04_16.zip">'
-     . '<img src="http://images.hamsterpaj.net/open_source/download_hamsterpaj.png" alt="En Hamsterpaj på 2.3MB" />'
-     . '</a>' . "\n";
+  echo 'Inom kort kommer komma ett verktyg som du kan använda för att fixa kod till Hamsterpaj. Sålänge finner du koden på <a href="http://github.com/Hamsterpaj/hamsterpaj/">Github</a>. Att lägga upp kod är än så länge inte möjligt.' . "\n";
   echo '</p>' . "\n";
   
   echo '<br />' . "\n";
-  
-  echo '<h3>Släppta koder</h3>' . "\n";
-	echo '<div id="open_source_file_structure">' . "\n";
-	echo '<h3>/storage/www/www.hamsterpaj.net/data/</h3>' . "\n";
-	echo open_source_get_file_tree($file_structure);
-	echo '</div>' . "\n";
-
-	echo '<br />' . "\n";
 
 	echo '<h3>Databas</h3>' . "\n";
 	echo '<p>' . "\n";
-	echo '<a href="http://217.118.208.249/phpmyadmin/">http://217.118.208.249/phpmyadmin/</a><br />' . "\n";
-	echo 'Användare: hp_structure<br />' . "\n";
-	echo 'Inget lösenord krävs' . "\n";
-	echo '</p>' . "\n";
-	
-	echo '<br />' . "\n";
-	
-	echo '<h3>När du är klar</h3>' . "\n";
-	echo '<p>' . "\n";
-	echo 'När du pillat färdigt lägger du upp koden i <a href="/diskussionsforum/hamsterpaj/open_source/">Open Source-forumet</a>.' . "\n";
+	//echo '<a href="http://217.118.208.249/phpmyadmin/">http://217.118.208.249/phpmyadmin/</a><br />' . "\n";
+	//echo 'Användare: hp_structure<br />' . "\n";
+	//echo 'Inget lösenord krävs' . "\n";
+	echo 'Kontakta en sysop på siten så fixar de en adress till dig där du kan tanka ner databasstruktur. OBS! Då MÅSTE få allt annat att funka (sajten i övrigt med kod och egenhostad server mm.) INNAN du frågar om detta.';
 	echo '</p>' . "\n";
 
 	open_source_bottom($open_source_config);
