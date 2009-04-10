16662d082e461daff29f05257aafec830050dabc
Patrick Lef <patrick@hamsterpaj.net>
2009-04-10 02:38:42 +0200

    Optimized ov statistics page with new SQL
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- admin/ov_watch.php
+++ admin/ov_watch.php
@@ -1,68 +1,49 @@
 <?php
+	require('../include/core/common.php');
 
-require('../include/core/common.php');
+	$ui_options['menu_path'] = array('admin', 'ov_watch');
+	$ui_options['stylesheets'][] = 'forms.css';
 
-$ui_options['menu_path'] = array('admin', 'ov_watch');
-$ui_options['stylesheets'][] = 'forms.css';
-
-
-if(!is_privilegied('use_statistic_tools'))
-{
-	$out .= 'inte för dig...';
-}
-else
-{
-	$query = 'SELECT user FROM privilegies GROUP BY(user)';
-	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
-	while ($data = mysql_fetch_assoc($result))
+	if(!is_privilegied('use_statistic_tools'))
 	{
-		$privilegied_users[] = $data['user'];
+		jscript_alert('Denna sida kräver privilegiet: use_statistic_tools');
+		jscript_location('/');
+		die('inte för dig...');
 	}
 	
-	$query = 'SELECT l.id, l.username, posts_removed, avatars_denied, avatars_approved FROM login AS l, admin_counts AS ac WHERE (l.id = ac.user_id) AND (';
-	$privilegied_users_count = count($privilegied_users);
-	$count = 0;
-	foreach ($privilegied_users as $user_id)
-	{
-		$query .= ' l.id = ' . $user_id;
-		$count++;
-		if ($privilegied_users_count != $count)
-		{
-			$query .= ' OR';
-		}
-	}
-	$query .= ') ORDER BY l.username';
+	$query = 'SELECT l.id, l.username, ac.avatars_denied, ac.avatars_approved, ac.posts_removed 
+						FROM privilegies AS p 
+						JOIN login AS l ON l.id = p.user AND l.is_removed = 0
+						LEFT JOIN admin_counts AS ac ON ac.user_id = p.user
+						WHERE p.privilegie IN ("igotgodmode","avatar_admin","discussion_forum_remove_posts")
+						GROUP BY p.user ORDER BY l.username';
+	
 	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
-	while($data = mysql_fetch_assoc($result))
-	{
-		$ovs[] = $data;
-	}
+	$ovs = mysql_fetch_assoc($result);
 	$out .= '<fieldset>' . "\n";
 	$out .= '<legend>Ordningsvakter och deras förehavanden</legend>' . "\n";
 	$out .= '<table class="form">' . "\n";
 	$out .= '<tr>' . "\n";
 	$out .= '<th>Namn</th>' . "\n";
-	$out .= '<th>Id</th>' . "\n";
 	$out .= '<th>Borttagna inlägg</th>' . "\n";
 	$out .= '<th>Nekade visningsbilder</th>' . "\n";
 	$out .= '<th>Validerade visningsbilder</th>' . "\n";
 	$out .= '<th>Andel nekade visningsbilder</th>' . "\n";
 	$out .= '</tr>' . "\n";
 	
-	foreach($ovs as $ov)
+	while($ov = mysql_fetch_assoc($result))
 	{
 		$out .= '<tr>' . "\n";
-		$out .= '<td>' . $ov['username'] . '</td>' . "\n";
-		$out .= '<td>' . $ov['id'] . '</td>' . "\n";
+		$out .= '<td><a href="/profile.php?user_id=' . $ov['id'] . '">' . $ov['username'] . '</a></td>' . "\n";
 		$out .= '<td>' . $ov['posts_removed'] . '</td>' . "\n";
 		$out .= '<td>' . $ov['avatars_denied'] . '</td>' . "\n";
 		$out .= '<td>' . $ov['avatars_approved'] . '</td>' . "\n";
-		// $out .= '<td>' . $ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) . '</td>' . "\n";
 		$out .= '<td>' . round($ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) * 100, 2).' % </td>' . "\n";
 		$out .= '</tr>' . "\n";
 	}
 	$out .= '</table>' . "\n";
-}	
+	$out .= '</fieldset>' . "\n";
+
 
 ui_top($ui_options);
 echo $out;
