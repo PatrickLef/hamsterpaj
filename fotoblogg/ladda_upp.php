fbcf1dddfe60f4acab23985a5c7b0064cf2d761c
Patrick Lef <patrick@hamsterpaj.net>
2009-04-26 00:19:45 +0200

    Made photoblog_upload_forbid adminlogged
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- fotoblogg/ladda_upp.php
+++ fotoblogg/ladda_upp.php
@@ -6,9 +6,9 @@
 	switch(isset($uri_parts[3]) ? $uri_parts[3] : '')
 	{
 		default:
-			if(mktime($_SESSION['photoblog_preferences']['upload_forbidden']) > time())
+			if($_SESSION['photoblog_preferences']['upload_forbidden'] > time())
 			{
-				$out .= '<h1>Du är avstängd från att ladda upp bilder fram tills: ' . $_SESSION['photoblog_preferences']['upload_forbidden']. '</h1>' . "\n";
+				$out .= '<h1>Du är avstängd från att ladda upp bilder fram tills: ' . date('Y:m:d H:i:s', $_SESSION['photoblog_preferences']['upload_forbidden']) . '</h1>' . "\n";
 				break;
 			}
 			$ui_options['stylesheets'][] = 'datepicker.css';
