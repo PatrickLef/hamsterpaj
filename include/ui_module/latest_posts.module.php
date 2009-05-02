07647eba122a421296b950440eed7e4fbc00c8ec
Patrick Lef <patrick@hamsterpaj.net>
2009-05-03 01:46:08 +0200

    Fixed so ui_modules doesn't show categories you are not allowed to read
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/latest_posts.module.php
+++ include/ui_module/latest_posts.module.php
@@ -5,9 +5,12 @@
 	$options['output'] .= '<ul>' . "\n";
 	foreach($threads as $thread)
 	{
-		$thread['title'] = (mb_strlen($thread['title'], 'UTF8') > 22) ? mb_substr($thread['title'], 0, 19, 'UTF8') . '...' : $thread['title'];
-		$info = 'I ' . $thread['category_title'] . ' av ' . $thread['username'] . ': ' . $thread['title'];
-		$options['output'] .= '<li><span class="ui_module_latest_posts_written">' . date('H:i', $thread['last_post_timestamp']) . '</span> <a title="' . $info . '" href="' . $thread['url'] . '">' . $thread['title'] . '</a></li>' . "\n";
+		if(forum_category_access($thread['read_threads']))
+		{
+			$thread['title'] = (mb_strlen($thread['title'], 'UTF8') > 22) ? mb_substr($thread['title'], 0, 19, 'UTF8') . '...' : $thread['title'];
+			$info = 'I ' . $thread['category_title'] . ' av ' . $thread['username'] . ': ' . $thread['title'];
+			$options['output'] .= '<li><span class="ui_module_latest_posts_written">' . date('H:i', $thread['last_post_timestamp']) . '</span> <a title="' . $info . '" href="' . $thread['url'] . '">' . $thread['title'] . '</a></li>' . "\n";
+		}
 	}
 	$options['output'] .= '</ul>' . "\n";
 
--- include/ui_module/latest_threads.module.php
+++ include/ui_module/latest_threads.module.php
@@ -3,9 +3,12 @@
 	$options['output'] .= '<ul>' . "\n";
 	foreach($threads AS $thread)
 	{
-		$thread['title'] = (mb_strlen($thread['title'], 'UTF8') > 22) ? htmlspecialchars(mb_substr(htmlspecialchars_decode($thread['title']), 0, 19, 'UTF8')) . '...' : $thread['title'];
-		$info = 'I ' . $thread['category_title'] . ' av ' . $thread['username'] . ': ' . $thread['title'];
-		$options['output'] .= '<li>' . date('H:i', $thread['timestamp']) . ' <a title="' . $info . '" href="' . $thread['url'] . '">' . $thread['title'] . '</a></li>' . "\n";
+		if(forum_category_access($thread['read_threads']))
+		{
+			$thread['title'] = (mb_strlen($thread['title'], 'UTF8') > 22) ? htmlspecialchars(mb_substr(htmlspecialchars_decode($thread['title']), 0, 19, 'UTF8')) . '...' : $thread['title'];
+			$info = 'I ' . $thread['category_title'] . ' av ' . $thread['username'] . ': ' . $thread['title'];
+			$options['output'] .= '<li>' . date('H:i', $thread['timestamp']) . ' <a title="' . $info . '" href="' . $thread['url'] . '">' . $thread['title'] . '</a></li>' . "\n";
+		}
 	}
 	$options['return'] .= '</ul>' . "\n";
 	
