d0b58c1993a0af5be0ace7716117ccb29cf1dda2
Erik Andersson <erik.rothoff@gmail.com>
2009-07-03 20:25:48 +0200

    The photoblog modules are now only shown when other photos are viewed. Do you think otherwise? Drop a comment@git.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- include/ui_module/photoblog_calendar.module.php
+++ include/ui_module/photoblog_calendar.module.php
@@ -1,3 +1,9 @@
-<?php	
-	$options['output'].= photoblog_calendar(PHOTOBLOG_CURRENT_USER, (int)PHOTOBLOG_CURRENT_MONTH, (int)PHOTOBLOG_CURRENT_YEAR); 
+<?php
+    if ( ! defined('PHOTOBLOG_CURRENT_USER') )
+    {
+	define('PHOTOBLOG_CURRENT_USER', $_SESSION['login']['id']);
+	define('PHOTOBLOG_CURRENT_MONTH', date('m'));
+	define('PHOTOBLOG_CURRENT_YEAR', date('Y'));
+    }
+    $options['output'].= photoblog_calendar(PHOTOBLOG_CURRENT_USER, (int)PHOTOBLOG_CURRENT_MONTH, (int)PHOTOBLOG_CURRENT_YEAR); 
 ?>
