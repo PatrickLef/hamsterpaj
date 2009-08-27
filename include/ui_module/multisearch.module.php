c56ce7c4e8f37ce82a5df41ee0424628370d3d2c
Patrick Lef <patrick@hamsterpaj.net>
2009-08-27 12:42:23 +0200

    Made searchmodule use new search function
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/multisearch.module.php
+++ include/ui_module/multisearch.module.php
@@ -1,6 +1,6 @@
 <?php
-	$options['output'] .= '<form action="/traffa/quicksearch.php">' . "\n";
-	$options['output'] .= '	<input type="text" id="ui_multisearch" value="Sök tjockis..." name="username" />' . "\n";
+	$options['output'] .= '<form action="/soek" method="get">' . "\n";
+	$options['output'] .= '	<input type="text" id="ui_multisearch" value="Sök på Hamsterpaj..." name="searchquery" />' . "\n";
 	$options['output'] .= '	<input type="submit" value="" class="button_magnifier" />' . "\n";
 	$options['output'] .= '</form>' . "\n";
 ?>
