a4ecbc9fb475e1deda998208ad84d946bb23785d
Erik Andersson <erik.rothoff@gmail.com>
2009-10-08 19:37:20 +0200

    Removed the possibility of double posts in groups Javascript
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- javascripts/groups.js
+++ javascripts/groups.js
@@ -4,23 +4,27 @@ $(document).ready(function(){
 		var group_message = $('#group_message').val();
 		var group_id = $(this).parent().attr('id'); // The forms ID
 		
+		$(this).attr('disabled', true);
+		
 		if(group_message == ''){
 			$('#form_notice').css("display", "none");
 			$('#form_notice').removeClass();
 			$('#form_notice').toggleClass('form_notice_error');
 			$('#form_notice').text('Du måste skriva in ett meddelande, dah?');
 			$('#form_notice').fadeIn(500);
+			$(this).attr('disabled', false);
 			return false;
 		}
 	
 		if(group_message == last_group_message)
 		{
 			$('#form_notice').css("display", "none");
-      $('#form_notice').removeClass();
-      $('#form_notice').toggleClass('form_notice_error');
-      $('#form_notice').text('DubbelpostarN');
-      $('#form_notice').fadeIn(500);
-      return false;
+			$('#form_notice').removeClass();
+			$('#form_notice').toggleClass('form_notice_error');
+			$('#form_notice').text('DubbelpostarN');
+			$('#form_notice').fadeIn(500);
+			$(this).attr('disabled', false);
+			return false;
 		}	
 	
 		$.ajax({
@@ -29,13 +33,14 @@ $(document).ready(function(){
 			data: 'groupid=' + group_id + '&group_message=' + encodeURIComponent(group_message),
 			timeout: 10000,
 			success: function(result) {
+				$('#group_message_submit').attr('disabled', false);
 				$('#posted_messages').prepend(result);
 				$('#group_message').attr("value", "");
-				$('#form_notice').css("display", "none");
-				$('#form_notice').removeClass();
-				$('#form_notice').toggleClass('form_notice_success');
-				$('#form_notice').text('Meddelandet skickat!');
-				$('#form_notice').fadeIn(500);
+				$('#form_notice').css("display", "none")
+					.removeClass()
+					.toggleClass('form_notice_success')
+					.text('Meddelandet skickat!')
+					.fadeIn(500);
 				$('#group_message').focus();
 				last_group_message = group_message;
 			}
